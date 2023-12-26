751e191b894fac788cb3b151604e3d17caaad56c
luiofff <95875204+luiofff@users.noreply.github.com>
2023-12-26 21:42:04 +0300

    fix adaptive bug

--- client/app/ui/titleBlock.tsx
+++ client/app/ui/titleBlock.tsx
@@ -7,9 +7,9 @@ export default function TitleBlock() {
         <>
             <div className='w-full h-56 grid place-items-center '>
                 <div className='flex flex-col gap-7 text-5xl font-bold'>
-                    <h1 className='text-white'>Консорциум -</h1>
+                    <h1 className='text-white text-4xl sm:text-5xl'>Консорциум -</h1>
                     <div className='flex gap-4 items-center'>
-                        <h1 className="font-medium text-white font-bold">это идеи </h1>
+                        <h1 className="text-white text-4xl sm:text-5xl">это идеи </h1>
                         <Image
                             src={sputnik}
                             alt="sputnik"
