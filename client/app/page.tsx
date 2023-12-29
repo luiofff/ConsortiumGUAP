3432bedf96eff459d96dd40d916c3bcd4ab27c93
luiofff <95875204+luiofff@users.noreply.github.com>
2023-12-30 00:26:16 +0300

    fix bug with company logo

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,18 @@
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from './ui/navbar/navbar'
 import TitleBlock from './ui/titleBlock'
 import Button from './ui/button/button'
+
+
+
+
+
+const compButtonsArray = [
+  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'twitter-button' },
+  { icon: 'yand', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'discord-button' },
+];
 
 export default function Home() {
   return (
@@ -10,7 +20,7 @@ export default function Home() {
       <Navbar />
       <TitleBlock />
       <div className={styles.content_space}>
-        <Button />
+        <Button companyButtons={compButtonsArray}/>
       </div>
     </>
   )
