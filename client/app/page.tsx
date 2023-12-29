f4f8209602bf50f3308e3405828061af0cece5b7
luiofff <95875204+luiofff@users.noreply.github.com>
2023-12-30 00:59:25 +0300

    add first work button

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -10,13 +10,11 @@ import Button from './ui/button/button'
 
 
 const compButtonsArray = [
-  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'north_east-button' },
-  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'north-button' },
-  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'north_west-button' },
-  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'west-button' },
-  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'west_south-button' },
-  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'south-button' },
-  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'south_east-button' },
+  { icon: 'gaz', label: 'Label 2', transitionDelay: '0.2s', countBtn: 'north-button' },
+  { icon: 'uiniversitu2035', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'north_east-button' },
+  { icon: 'yand', label: 'Label 2', transitionDelay: '0.3s', countBtn: 'north_west-button' },
+  { icon: 'megafon', label: 'Label 2', transitionDelay: '0.4s', countBtn: 'west-button' },
+  { icon: 'kirov_zavod', label: 'Label 2', transitionDelay: '0s', countBtn: 'east-button' },
 ];
 
 export default function Home() {
