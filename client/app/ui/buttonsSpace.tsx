be8a454319c57c0b4865fb5b001aa50fd4af1f60
luiofff <95875204+luiofff@users.noreply.github.com>
2024-01-04 00:59:05 +0300

    add all company logos

--- client/app/ui/buttonsSpace.tsx
+++ client/app/ui/buttonsSpace.tsx
@@ -23,10 +23,42 @@ const compButtonsArray = [
         { icon: 'fixar', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'north_east-button' },
     ],
     [
-        { icon: 'gaz', label: 'Label 2', transitionDelay: '0.2s', countBtn: 'north-button' },
-        { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'north_east-button' },
-        { icon: 'samsungIT', label: 'Label 2', transitionDelay: '0.3s', countBtn: 'north_west-button' },
-        { icon: 'gaz', label: 'Label 2', transitionDelay: '0.4s', countBtn: 'west-button' }
+        { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'west_south-button' },
+        { icon: 'granit', label: 'Label 2', transitionDelay: '0s', countBtn: 'west-button' },
+        { icon: 'samsungIT', label: 'Label 2', transitionDelay: '0.2s', countBtn: 'south-button' },
+        { icon: 'cloud', label: 'Label 2', transitionDelay: '0.3s', countBtn: 'south_east-button' }
+    ],
+    [
+        { icon: 'mgbot', label: 'Label 2', transitionDelay: '0s', countBtn: 'north_east-button' },
+        { icon: 'spb', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'north-button' },
+        { icon: 'volts', label: 'Label 2', transitionDelay: '0.2s', countBtn: 'north_west-button' },
+        { icon: 'infotecs', label: 'Label 2', transitionDelay: '0.3s', countBtn: 'west-button' },
+        { icon: 'nppkt', label: 'Label 2', transitionDelay: '0.4s', countBtn: 'west_south-button' }
+    ],
+    [
+        { icon: 'mars', label: 'Label 2', transitionDelay: '0s', countBtn: 'north_east-button' },
+        { icon: 'kirov_zavod', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'east-button' },
+        { icon: 'yand', label: 'Label 2', transitionDelay: '0.2s', countBtn: 'south_east-button' }
+    ],
+    [
+        { icon: 'spbficran', label: 'Label 2', transitionDelay: '0s', countBtn: 'west-button' },
+    ],
+    [
+        { icon: 'gaz', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'east-button' },
+        { icon: 'uiniversitu2035', label: 'Label 2', transitionDelay: '0s', countBtn: 'south_east-button' },
+        { icon: 'yand', label: 'Label 2', transitionDelay: '0.2s', countBtn: 'north_east-button' },
+        { icon: 'megafon', label: 'Label 2', transitionDelay: '0.4s', countBtn: 'north_west-button' },
+        { icon: 'kirov_zavod', label: 'Label 2', transitionDelay: '0.3s', countBtn: 'north-button' }
+    ],
+    [
+        { icon: 'silmash', label: 'Label 2', transitionDelay: '0s', countBtn: 'west-button' },
+    ],
+    [
+        { icon: 'gaz', label: 'Label 2', transitionDelay: '0.3s', countBtn: 'south-button' },
+        { icon: 'uiniversitu2035', label: 'Label 2', transitionDelay: '0.4s', countBtn: 'south_east-button' },
+        { icon: 'yand', label: 'Label 2', transitionDelay: '0s', countBtn: 'north_west-button' },
+        { icon: 'megafon', label: 'Label 2', transitionDelay: '0.1s', countBtn: 'west-button' },
+        { icon: 'kirov_zavod', label: 'Label 2', transitionDelay: '0.2s', countBtn: 'west_south-button' }
     ],
 ];
 
@@ -50,15 +82,24 @@ export default function ButtonsSpace() {
       <div className="flex">
         <Button companyButtons={compButtonsArray[4]} title="Лаборатория искусственного интеллекта"/>
       </div>
-      {/* <div className="flex flex-row-reverse relative">
+      <div className="flex flex-row-reverse relative">
         <Button companyButtons={compButtonsArray[5]} title="Лаборатория кибербезопасности ГУАП-Infowatch"/>
       </div>
-      <div className="flex">
+      <div className="flex flex-row-reverse relative pr-40 pt-10">
         <Button companyButtons={compButtonsArray[6]} title="Лаборатория электроэнергетики"/>
       </div>
-      <div className="flex flex-row-reverse relative">
+      <div className="flex">
         <Button companyButtons={compButtonsArray[7]} title="Отдел Инженерный гараж"/>
-      </div> */}
+      </div>
+      <div className="flex flex-row-reverse relative">
+        <Button companyButtons={compButtonsArray[8]} title="Лаборатория автоматизации технологических процессов"/>
+      </div>
+      <div className="flex">
+        <Button companyButtons={compButtonsArray[9]} title="Студенческое конструкторское бюро 'Силовые машины ГУАП'"/>
+      </div>
+      <div className="flex flex-row-reverse relative">
+        <Button companyButtons={compButtonsArray[10]} title="Лаборатория технологического предпринимательства"/>
+      </div>
     </div>
   );
 }
