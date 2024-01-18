c5dd8704d524dd54678314b9eed8ed16f237ae1e
luiofff <95875204+luiofff@users.noreply.github.com>
2024-01-18 23:27:41 +0300

    Update button.tsx

--- client/app/ui/button/button.tsx
+++ client/app/ui/button/button.tsx
@@ -47,7 +47,7 @@ export default function Button({ companyButtons, title, elem, projects }: Button
   
     return (
         <>
-            <div className={styles.btn_block}>
+            {/* <div className={styles.btn_block}>
                 {
                     clicked ? 
                     (   
@@ -122,7 +122,44 @@ export default function Button({ companyButtons, title, elem, projects }: Button
                 </div>
 
                 
-            </div>  
+            </div>   */}
+            
+            <div className={styles.btn_block}>
+                <div id={elem} className="grid place-items-center z-50">
+                            <div ref={fullBtnRef}  className={styles.full_btn}>
+                                <div className="flex flex-col gap-6 justify-center items-center mobile:gap-2 ">
+                                    <span style={{ color:"#fff"}} className="flex justify-center text-center w-24 mobile:text-xs">{title}</span>
+                                    <span style={{ color:"#fff" }} className="modile:text-xs">Партнеры</span>
+                                    <div  className="flex gap-4 justify-center">
+                                        {
+                                            companyButtons.map((button, index) => (
+                                                <CompanyLogo company={button.icon} />
+                                            )  
+                                        )
+                                        }
+                                    </div>
+                                    <span style={{ color:"#fff" }} className="modile: text-xs">Проекты</span>
+                                    {
+                                        projects.map((project, index) => (
+                                            <div key={index} className="flex gap-4 w-48 justify-center">
+                                            <div style={{ fontSize: "0.4rem", border: "2px solid #F92D77" }} className="p-9 text-center grid place-items-center rounded-full border-2 mobile:p-6">
+                                                <span className="absolute text-center grid place-items-center w-14">{project.left}</span>
+                                            </div>
+                                            <div style={{ fontSize: "0.4rem", border: "2px solid #1546FA" }} className="p-9 text-center grid place-items-center rounded-full border-2 mobile:p-6">
+                                                <span className="absolute text-center grid place-items-center w-14">{project.center}</span>
+                                            </div>
+                                            <div style={{ fontSize: "0.4rem", border: "2px solid #15C1EC" }} className="p-9 text-center grid place-items-center rounded-full border-2 mobile:p-6">
+                                                <span className="absolute text-center grid place-items-center w-14">{project.rigth}</span>
+                                            </div>
+                                            </div>
+                                        ))
+                                    }
+                                    
+                                    <button style={{ color:"#fff" }} className="modile: text-xs">Подробнее →</button>
+                                </div>
+                            </div>
+                        </div>
+            </div>
         </>
     );
 }
